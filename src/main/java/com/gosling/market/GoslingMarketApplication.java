e02641ad0ff234f8a0bb7b993f0c617040bca34e
hgarciaospina <henrygarciaospina@gmail.com>
2023-04-26 22:41:00 -0500

    Estructura proyecto

---
+++ src/main/java/com/gosling/market/GoslingMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gosling.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoslingMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoslingMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gosling/market/GoslingMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosling.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoslingMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
