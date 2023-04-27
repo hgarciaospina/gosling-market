934a3a71c59ae9d77671689c0d9cfeb568855c4a
hgarciaospina <henrygarciaospina@gmail.com>
2023-04-27 08:50:14 -0500

    Definicion CategoriaDTO

---
+++ src/main/java/com/gosling/market/domain/dto/CategoriaDTO.java
@@ -0,0 +1,31 @@
+package com.gosling.market.domain.dto;
+
+public class CategoriaDTO {
+    private int idCategoria;
+    private String descripcion;
+    private boolean estado;
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
