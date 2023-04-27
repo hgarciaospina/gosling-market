b901df23d04afa2a0f16cb70dde79664508a26fb
hgarciaospina <henrygarciaospina@gmail.com>
2023-04-27 09:11:55 -0500

    Definición de IProductoRepositorio

--- src/main/java/com/gosling/market/domain/dto/ProductoDTO.java
+++ src/main/java/com/gosling/market/domain/dto/ProductoDTO.java
@@ -1,2 +1,72 @@
-package com.gosling.market.domain.dto;public class ProductoDTO {
+package com.gosling.market.domain.dto;
+
+import com.gosling.market.persistence.entity.Categoria;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Size;
+
+public class ProductoDTO {
+    private int idProducto;
+    private String descripcion;
+    private int idCategoria;
+    private Categoria categoria;
+    private double precioVenta;
+    private int cantidadStock;
+    private boolean estado;
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public int getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(int cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 }
---
+++ src/main/java/com/gosling/market/domain/repository/IProductoRepositorio.java
@@ -0,0 +1,17 @@
+package com.gosling.market.domain.repository;
+
+import com.gosling.market.domain.dto.ProductoDTO;
+import com.gosling.market.persistence.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IProductoRepositorio {
+
+    List<ProductoDTO> getAll();
+    Optional <List<ProductoDTO>> getByCategoria(int idCategoria);
+    Optional<List<ProductoDTO>> getStockBajo(int cantidadStock);
+    Optional<ProductoDTO> getProducto(int idProducto);
+    Producto save(ProductoDTO productoDTO);
+    void delete(int idProducto);
+}
